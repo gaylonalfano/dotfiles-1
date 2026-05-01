d339b1ff3844d7a5e0135a321e98554572f37891
Jongwook Choi <wookayin@gmail.com>
2026-04-30 21:41:41 -0700

    vim(ftplugin/tmux): use tabsize 2, and marker-based folding

--- nvim/after/ftplugin/tmux.lua
+++ nvim/after/ftplugin/tmux.lua
@@ -1,2 +1,8 @@
 require("config.treesitter").ensure_parsers_installed("tmux")
 require("config.treesitter").setup_highlight("tmux")
+
+vim.opt_local.ts = 2
+vim.opt_local.sts = 2
+vim.opt_local.sw = 2
+
+vim.opt_local.foldmethod = 'marker'
